f699c1ecebcfca9be3aad7c1ae29c454fdb44f5f
Afonso Dias <afonso-dias2011@hotmail.com>
2017-04-06 17:36:29 -0300

    Criando URls do core e setando o app nos settings.

--- source/ES012017/core/views.py
+++ source/ES012017/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'core/index.html'
+    context_object_name = {}
+
+    def get_queryset(self):
+        return #vai retornar algum objeto para ser usado
