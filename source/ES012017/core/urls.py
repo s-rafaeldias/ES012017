e88aaf60c49373e78df470213e3d8f8085b90127
Rafael Dias Silveira <rafa197795@gmail.com>
2017-04-12 19:34:48 -0300

    Modificação dos nomes das urls

--- source/ES012017/core/urls.py
+++ source/ES012017/core/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
 
     #Urls para o CRUD de Emprego
-    url(r'^newEmprego/$', views.EmpregoCreate.as_view(), name='emprego_create'),
-    url(r'^editEmprego/(?P<pk>\d+)/$', views.EmpregoUpdate.as_view(), name='emprego_update'),
-    url(r'^deleteEmprego/(?P<pk>\d+)/$', views.EmpregoDelete.as_view(), name='emprego_delete'),
-    url(r'^viewEmprego/(?P<pk>\d+)/$', views.EmpregoDetailView.as_view(), name='emprego_detail'),
-    url(r'^listEmprego/$', views.EmpregoListView.as_view(), name='emprego_list'),
+    url(r'^newEmprego/$', views.EmpregoCreate.as_view(), name='emprego-create'),
+    url(r'^editEmprego/(?P<pk>\d+)/$', views.EmpregoUpdate.as_view(), name='emprego-update'),
+    url(r'^deleteEmprego/(?P<pk>\d+)/$', views.EmpregoDelete.as_view(), name='emprego-delete'),
+    url(r'^viewEmprego/(?P<pk>\d+)/$', views.EmpregoDetailView.as_view(), name='emprego-detail'),
+    url(r'^listEmprego/$', views.EmpregoListView.as_view(), name='emprego-list'),
 
 ]
