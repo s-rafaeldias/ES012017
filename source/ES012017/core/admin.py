b812ebeb19197d01d7d8ac60adc5445e58306907
Afonso Dias <afonso-dias2011@hotmail.com>
2017-04-15 01:10:44 -0300

    Adicionando "Emprego" no admin e comentando teste incompleto

--- source/ES012017/core/admin.py
+++ source/ES012017/core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import PerfilUsuario
+from .models import PerfilUsuario, Emprego
 # Register your models here.
 admin.site.register(PerfilUsuario)
+admin.site.register(Emprego)
