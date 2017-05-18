e19b0fcaf4dbfeeb6d7f531cf1aa20c79d69da7e
Afonso Dias <afonso-dias2011@hotmail.com>
2017-05-18 00:45:52 -0300

    Trocando "Emprego" por "Freelas" + Apagando campos desnecessarios

--- core/models.py
+++ core/models.py
@@ -6,20 +6,13 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class Emprego(models.Model):
-    nome             = models.CharField(max_length=100)
-    descricao        = models.CharField(max_length=250)
-    empresa          = models.CharField(max_length=100)
-    area_atuacao     = models.CharField(max_length=100)
-    local_trabalho   = models.CharField(max_length=100)
-    quantidade_vagas = models.PositiveIntegerField()
+class Freela(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=250)
+    local_trabalho = models.CharField(max_length=100)
     jornada_trabalho = models.PositiveIntegerField()
-    salario          = models.DecimalField(max_digits=8, decimal_places=2)
-    atribuicoes      = models.TextField()
-    status           = models.BooleanField()
-    # Provavel tabela nova
-    # area_formacao
-    # conhecimentos_exigidos
+    remuneracao = models.DecimalField(max_digits=8, decimal_places=2)
+    status = models.BooleanField()
 
     def __str__(self):
         return self.nome
@@ -27,21 +20,16 @@ class Emprego(models.Model):
     def __unicode__(self):
         return self.nome
 
+
 class User(AbstractUser):
     GENRE_CHOICES = (
-        ('m', 'Masculino'),
-        ('f', 'Feminino'),
-    )
-
-    USER_TYPE = (
-        ('e', 'Empresa'),
-        ('t', 'Trabalhador'),
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('N', 'Nao aplicavel'),
     )
     descricao = models.CharField(max_length=250)
-    cpf = models.CharField(max_length=45, blank=True, null=True)
-    cnpj = models.CharField(max_length=45, blank=True, null=True)
+    cpf_cnpj = models.CharField(max_length=45, blank=True, null=True)
     genero = models.CharField(max_length=1, blank=True, null=True, choices=GENRE_CHOICES)
-    perfil = models.CharField(max_length=1, choices=USER_TYPE)
     telefone = models.CharField(max_length=45, blank=True, null=True)
     endereco = models.CharField(max_length=120)
-    empregos = models.ManyToManyField(Emprego)
+    freela = models.ManyToManyField(Freela)
